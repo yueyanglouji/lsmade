f42c7ba6922df11ed857a38bfcacd7bd5f0961b5
Xiaobao.Li <Xiaobao.Li@cn.yokogawa.work>
2021-02-07 14:20:49 +0800

    update

--- src/v4.0/styles/cssframework/ls/vue/title/src/Title.vue
+++ src/v4.0/styles/cssframework/ls/vue/title/src/Title.vue
@@ -3,7 +3,7 @@
 <script>
     export default {
         name: "LsTitle",
-        props: [ 'value', 'saveToStore' ],
+        props: [ 'value', 'saveToStore', 'pageId' ],
         methods:{
             clickFunc(func, data){
                 if(func){
@@ -18,6 +18,37 @@
                     this.$store.state.lsMade.title = this.$props.value
                 }
                 window.document.title = this.$props.value
+                if(this.$props['pageId']){
+                    const cl = window.document.body.className
+                    const pageId = "lsmade-" + this.$props['pageId'] + " " + this.$props['pageId']
+                    const removeId = []
+                    const result = []
+                    if(cl){
+                        cl.split(" ")
+                        for(let i=0;i<cl.length;i++){
+                            const ccl = cl[i]
+                            if(/lsmade-.*/.test(ccl)){
+                                const p_id = ccl.substr(7)
+                                removeId.push(ccl)
+                                removeId.push(p_id)
+                            }
+                        }
+                        for(let i=0;i<cl.length;i++){
+                            const ccl = cl[i]
+                            if(!removeId.contains(ccl)){
+                                result.push(ccl)
+                            }
+                        }
+                        let result_class = removeId.join(" ")
+                        if(result_class){
+                            result_class += " "
+                        }
+                        window.document.body.className = result_class + pageId
+                    }else{
+                        window.document.body.className = pageId
+                    }
+
+                }
             }
         },
         watch: {
